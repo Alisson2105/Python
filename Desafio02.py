29fe827b4c1ec0265e593da371f1b2dc283565d9
Alisson_Souza <alisson.erga@outlook.com>
2025-01-14 18:53:25 -0300

    Segundo desafio

---
+++ Desafio02.py
@@ -0,0 +1,6 @@
+print("===== DESAFIO 02 =====")
+dia = input('- Informe o dia que você nasceu: ')
+mes = input('- Informe o mês que você nasceu: ')
+ano = input('- Informe o ano que você nasceu: ')
+
+print(f"- Você nasceu no dia{dia} de {mes} de {ano}. Correto?")
