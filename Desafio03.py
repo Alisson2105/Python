d9ee0c8822b090b0f722012b93a35868c86bbfcc
Alisson_Souza <alisson.erga@outlook.com>
2025-01-14 19:07:45 -0300

    Terceiro desafio

---
+++ Desafio03.py
@@ -0,0 +1,8 @@
+print("===== DESAFIO 03 =====")
+
+numero1 = int(input("- Informe o primeiro número: "))
+numero2 = int(input("- Informe o segundo número: "))
+soma = numero1 + numero2
+
+print(f"- A soma é {soma}")
+
